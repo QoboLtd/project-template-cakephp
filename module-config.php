3df32ae0d789d124e3cbb2e16510183f547b7b2d
Iacovos Constantinou <softius@gmail.com>
2020-05-26 12:22:08 +0300

    Remove example file (task #16415)

--- module-config.php
+++
@@ -1,9 +0,0 @@
-
-`(new ModuleConfig(ConfigType::MODULE(), $this->moduleName, null, $this->moduleConfigOptions))->parseToArray();`
-
-
-$migration = (new ModuleConfig(ConfigType::MIGRATION(), $this->moduleName, null, $this->moduleConfigOptions))->parseToArray();
-$lists = $this->getModuleLists();
-$fields = (new ModuleConfig(ConfigType::FIELDS(), $this->moduleName, null, $this->moduleConfigOptions))->parseToArray();
-$menus = (new ModuleConfig(ConfigType::MENUS(), $this->moduleName, null, $this->moduleConfigOptions))->parseToArray();
-$views = $this->getModuleViews();
